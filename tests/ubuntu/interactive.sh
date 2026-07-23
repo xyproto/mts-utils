65e3612f335249faec2474327e56c2dedc0dd1af
Alexander F. Rødseth <rodseth@gmail.com>
2026-07-23 10:45:30 +0200

    Update tests

--- tests/ubuntu/interactive.sh
+++ tests/ubuntu/interactive.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build --no-cache -t cxx:ubuntu_18_10 .
-docker run --rm -it --name cxx_ubuntu_interactive cxx:ubuntu_18_10 bash
+docker build --no-cache -t slay:ubuntu_24_04 .
+docker run --rm -it --name slay_ubuntu_interactive slay:ubuntu_24_04 bash
--- tests/ubuntu/run.sh
+++ tests/ubuntu/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$scriptdir"
-docker build --no-cache -t cxx:ubuntu_18_10 . && docker run --rm --name cxx_ubuntu cxx:ubuntu_18_10
+docker build --no-cache -t slay:ubuntu_24_04 . && docker run --rm --name slay_ubuntu slay:ubuntu_24_04
